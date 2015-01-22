f83a952539e78c67695cbe4e80ce557d7f450217
Debbie Cravey & Jay Sivakumaran <student@epicodus.com>
2015-01-22 15:23:11 -0800

    spending_by_category functionality added

--- lib/category.rb
+++ lib/category.rb
@@ -17,6 +17,16 @@ class Category
     returned_categories
   end
 
+  define_singleton_method(:find) do |id|
+    Category.all().each do |category|
+      if(category.id() == id)
+        @found_category = category
+      end
+    end
+    @found_category
+  end
+
+
   define_method(:save) do
     result = DB.exec("INSERT INTO categories (type) VALUES ('#{@type}') RETURNING id;")
     @id = result.first().fetch("id").to_i()
@@ -26,14 +36,21 @@ class Category
     self.type().==(another_category.type())
   end
 
-  define_singleton_method(:find) do |id|
-    Category.all().each do |category|
-      if(category.id() == id)
-        @found_category = category
-      end
+  define_method(:spending_by_category) do
+    id1 = self.id
+    total = 0;
+    result = DB.exec("SELECT expenses.* FROM
+    categories JOIN expenses_categories ON (categories.id = expenses_categories.category_id)
+    JOIN expenses ON (expenses_categories.expense_id = expenses.id)
+    WHERE #{@id} = expenses_categories.category_id;")
+
+    result.each() do |expense|
+      total += expense.fetch("amount").to_f
+      binding.pry
     end
-    @found_category
+    total
   end
+
 
 
 end
--- spec/category_spec.rb
+++ spec/category_spec.rb
@@ -20,4 +20,21 @@ describe(Category) do
       expect(Category.find(test_category.id())).to(eq(test_category))
     end
   end
+
+  describe("#spending_by_category") do
+    it("returns the total amount that was spent in category") do
+      test_category = Category.new({:id => nil, :type => "lunch"})
+      test_category.save()
+      expense = Expense.new({:id => nil, :description => "eat out", :amount => 12.50, :company => "Mr Taco", :day => '2015-06-24'})
+      expense2 = Expense.new({:id => nil, :description => "eat out", :amount => 7.50, :company => "Subway", :day => '2015-06-28'})
+      expense.save()
+      expense2.save()
+      spending1 = Spending.new(:id => nil, :expense_id => expense.id, :category_id => test_category.id)
+      spending1.save()
+      spending2 = Spending.new(:id => nil, :expense_id => expense2.id, :category_id => test_category.id)
+      spending2.save()
+      total = test_category.spending_by_category()
+      expect(total).to eq 20
+    end
+  end
 end
